5c2adf72ae40c04946e67e4ac464b55185b7cf98
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-06-19 09:54:06 -0500

    submitted solution

--- src/indexOf.js
+++ src/indexOf.js
@@ -1,4 +1,28 @@
+//TODO implement a binary search algorithm that passes the tests.
+
 function indexOf(compare, sortedElements) {
+  if (Array.isArray(sortedElements)) {
+    let lowerIndex = 0;
+    let upperIndex = sortedElements.length - 1;
+
+    while (lowerIndex <= upperIndex) {
+      const index = Math.floor((upperIndex + lowerIndex) / 2);
+
+      const comparison = compare(sortedElements[index], index, sortedElements);
+
+      if (comparison > 0) {
+        lowerIndex = index + 1;
+      }
+
+      if (comparison === 0) {
+        return index;
+      }
+
+      if (comparison < 0) {
+        upperIndex = index - 1;
+      }
+    }
+  }
   return -1;
 }
 
